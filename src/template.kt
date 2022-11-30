a23a720f00846023cb3cec91ff03cac0ec1a8a1f
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-11-30 20:36:54 +0100

    further preparations for day 1

---
+++ src/template.kt
@@ -0,0 +1,14 @@
+/* Template file for all days, may need to change 🤔*/
+fun main(){
+    fun part1(input:List<String>){
+    }
+
+    fun part2(input: List<String>){
+    }
+
+//    val input = readInput("day1/input")      //SUBMITTING
+    val input = readInput("day1/testInput")  //TESTING
+
+    println("Part 1 result " + part1(input))
+    println("Part 2 result " + part2(input))
+}
