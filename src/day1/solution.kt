72b752f759ca2fe9c10d5fcf1e6e5d17cecbdce4
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-12-01 07:28:36 +0100

    🎄finished day 1 2022

--- src/day1/solution.kt
+++ src/day1/solution.kt
@@ -1,8 +1,43 @@
 fun main(){
-    fun part1(input:List<String>){
+    fun part1(input:List<String>): Int{
+        var mostCalories = 0
+        var currentCalories = 0
+
+        for (calorie in input){
+            try{
+                currentCalories += calorie.toInt()
+                if (currentCalories > mostCalories){
+                    mostCalories = currentCalories
+                }
+            } catch (e: NumberFormatException){
+                currentCalories = 0
+            }
+        }
+        return mostCalories
     }
 
-    fun part2(input: List<String>){
+    fun part2(input: List<String>) : Int{
+        var mostCalories = arrayOf(0, 0, 0)
+        var currentCalories = 0
+
+        for (calorie in input + ""){
+            try{
+                currentCalories += calorie.toInt()
+            } catch (e: NumberFormatException){
+                var lowest = mostCalories.indexOf(mostCalories.min())
+                if (currentCalories > mostCalories[lowest]) {
+                    mostCalories[lowest] = currentCalories
+                }
+                currentCalories = 0
+            }
+        }
+
+        var sum = 0
+        for (i in mostCalories){
+            sum += i
+        }
+        return sum
+
     }
 
 //    val input = readInput("day1/input")      //SUBMITTING
